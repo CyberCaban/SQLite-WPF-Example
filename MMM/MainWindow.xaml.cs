670d91bb3462cb8d520a2683826790b4b0fd4c33
d.konovalov <d.konovalov@PC210-06>
2025-05-13 10:39:45 +0300

    Add project files.

---
+++ MMM/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SSS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DBState DBState = new DBState();
+        List<TodoItem> TodoItems = new List<TodoItem>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateUI();
+        }
+        private void UpdateUI()
+        {
+            TodoItems = DBState.GetItems();
+            foreach (TodoItem item in TodoItems)
+            {
+                Console.WriteLine(item);
+            }
+            TodoListBox.ItemsSource = null;
+            TodoListBox.ItemsSource = TodoItems;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextBox.Text.Trim()))
+            {
+                var newItem = new TodoItem { Title = TextBox.Text, IsCompleted = false };
+                DBState.InsertOrReplace(newItem);
+                TextBox.Text = "";
+                TextBox.Focus();
+                UpdateUI();
+            }
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            var checkbox = sender as CheckBox;
+            var item = checkbox.DataContext as TodoItem;
+            DBState.InsertOrReplace(item);
+            UpdateUI();
+
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (TodoListBox.SelectedItem != null)
+            {
+                DBState.DeleteItem(TodoListBox.SelectedItem as TodoItem);
+                UpdateUI();
+            }
+        }
+    }
+}
---
+++ MMM/TodoItem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Data.SQLite;
+using System.IO;
+using ServiceStack.OrmLite;
+using ServiceStack.DataAnnotations;
+
+namespace SSS
+{
+    [Alias("todos")]
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Title { get; set; }
+        public TodoItem() { }
+        public override string ToString()
+        {
+            return $"TodoItem {{ Id: {Id}, Title: {Title}, IsCompleted: {IsCompleted} }}";
+        }
+    }
+
+    public class DBState
+    {
+        private OrmLiteConnectionFactory _connection;
+        private string _filePath;
+        public DBState()
+        {
+            var dbPath = Directory.GetCurrentDirectory() + "todos.db";
+            _connection = new OrmLiteConnectionFactory(dbPath, SqliteDialect.Provider);
+            _filePath = dbPath;
+            using (var db = _connection.Open())
+            {
+                db.CreateTableIfNotExists<TodoItem>();
+            }
+        }
+
+        public List<TodoItem> GetItems()
+        {
+            List<TodoItem> list = new List<TodoItem>();
+            using (var db = _connection.Open())
+            {
+                list = db.Select<TodoItem>().ToList();
+            }
+            return list;
+        }
+        public void InsertOrReplace(TodoItem item)
+        {
+            using (var db = _connection.Open())
+            {
+                if (item.Id != 0) { db.Update(item); }
+                else { db.Insert(item); }
+            }
+        }
+        public void DeleteItem(TodoItem item)
+        {
+            using (var db = _connection.Open()) { db.Delete(item); }
+        }
+    }
+}
